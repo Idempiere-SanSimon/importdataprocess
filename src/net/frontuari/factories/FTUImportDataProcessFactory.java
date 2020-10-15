d7335e11febff661e102142cd2eef5c306f93202
David Castillo <root@funcional1>
2020-10-15 17:43:51 -0400

    Import Inventory changes

--- src/net/frontuari/factories/FTUImportDataProcessFactory.java
+++ src/net/frontuari/factories/FTUImportDataProcessFactory.java
@@ -5,6 +5,9 @@ import org.compiere.process.ProcessCall;
 
 import net.frontuari.process.ImportBPBankAccount;
 import net.frontuari.process.ImportBPartner;
+
+import net.frontuari.process.ImportGLJournal;
+import net.frontuari.process.ImportInventory;
 import net.frontuari.process.ImportInvoice;
 import net.frontuari.process.ImportPayment;
 import net.frontuari.process.ImportPriceList;
@@ -32,6 +35,15 @@ public class FTUImportDataProcessFactory implements IProcessFactory {
 		
 		if(className.equals("net.frontuari.process.ImportInvoice"))
 			return new ImportInvoice();
+		
+	/*	if(className.equals("net.frontuari.process.ImportFixedAsset"))
+			return new ImportFixedAsset();*/
+		
+		if(className.equals("net.frontuari.process.ImportInventory"))
+			return new ImportInventory();
+		
+		if(className.equals("net.frontuari.process.ImportGLJournal"))
+			return new ImportGLJournal();
 		
 		return null;
 	}
