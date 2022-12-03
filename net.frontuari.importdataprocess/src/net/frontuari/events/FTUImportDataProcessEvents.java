e876dc569c0c918a5fc498d858480adf0bf3291a
Jorge Colmenarez <jlct.master@gmail.com>
2022-12-03 08:28:37 -0400

    Fixed bug with events Journal and import Employee

--- net.frontuari.importdataprocess/src/net/frontuari/events/FTUImportDataProcessEvents.java
+++ net.frontuari.importdataprocess/src/net/frontuari/events/FTUImportDataProcessEvents.java
@@ -4,6 +4,7 @@ import org.adempiere.base.event.IEventTopics;
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MElementValue;
 import org.compiere.model.MJournalLine;
+import org.compiere.model.MSysConfig;
 import org.compiere.model.PO;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
@@ -23,7 +24,7 @@ public class FTUImportDataProcessEvents extends FTUEvent{
 		if (po.get_TableName().equals(MJournalLine.Table_Name)) {
 			MJournalLine line = (MJournalLine) po;
 			if (type.equals(IEventTopics.PO_BEFORE_NEW) || type.equals(IEventTopics.PO_BEFORE_CHANGE)) {
-				if (line.getAccount_ID()>0 && line.getC_Activity_ID() > 0 && line.getUser1_ID() > 0) {
+				if (MSysConfig.getBooleanValue("checkInitialAccountNo", false, line.getAD_Client_ID()) && line.getAccount_ID()>0 && line.getC_Activity_ID() > 0 && line.getUser1_ID() > 0) {
 				MElementValue ev = new MElementValue(line.getCtx(), line.getAccount_ID(), line.get_TrxName());
 				String values = DB.getSQLValueString(line.get_TrxName(), "SELECT AccountValue FROM FTU_Activity_User1_Access WHERE "
 						+ " C_Activity_ID = ? AND User1_ID = ? ", line.getC_Activity_ID(), line.getUser1_ID());
