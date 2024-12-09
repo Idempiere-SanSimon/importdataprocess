95b5ee8e0e424e2ed582b32a5a296a5be0ca1b6f
Jorge Colmenarez <jlct.master@gmail.com>
2024-12-09 17:15:49 -0400

    Fixed User1 Error

--- net.frontuari.importdataprocess/src/net/frontuari/process/ImportInvoice.java
+++ net.frontuari.importdataprocess/src/net/frontuari/process/ImportInvoice.java
@@ -588,36 +588,37 @@ public class ImportInvoice extends CustomProcess
 			log.warning ("Invalid C_1099Box_Value=" + no);
 		
 		// Set Conversion Type
-				sql = new StringBuilder ("UPDATE I_Invoice o ")
-						.append("SET C_ConversionType_ID=(SELECT C_ConversionType_ID FROM C_ConversionType a")
-						.append(" WHERE o.ConversionTypeValue=a.Value AND (a.AD_Client_ID = o.AD_Client_ID OR a.AD_Client_ID = 0)) ")
-						.append(" WHERE C_ConversionType_ID IS NULL and ConversionTypeValue IS NOT NULL")
-						.append(" AND I_IsImported<>'Y'").append (clientCheck);
-				no = DB.executeUpdate(sql.toString(), get_TrxName());
-				log.fine("Set C_ConversionType_ID=" + no);
-				sql = new StringBuilder ("UPDATE I_Invoice ")
-						.append("SET I_IsImported='E', I_ErrorMsg=I_ErrorMsg||'ERR=Invalid ConversionTypeValue, ' ")
-						.append("WHERE C_ConversionType_ID IS NULL AND (ConversionTypeValue IS NOT NULL)")
-						.append(" AND I_IsImported<>'Y' ").append (clientCheck);
-				no = DB.executeUpdate(sql.toString(), get_TrxName());
-				if (no != 0)
-					log.warning ("Invalid ConversionTypeValue=" + no);
-				
+		sql = new StringBuilder ("UPDATE I_Invoice o ")
+				.append("SET C_ConversionType_ID=(SELECT C_ConversionType_ID FROM C_ConversionType a")
+				.append(" WHERE o.ConversionTypeValue=a.Value AND (a.AD_Client_ID = o.AD_Client_ID OR a.AD_Client_ID = 0)) ")
+				.append(" WHERE C_ConversionType_ID IS NULL and ConversionTypeValue IS NOT NULL")
+				.append(" AND I_IsImported<>'Y'").append (clientCheck);
+		no = DB.executeUpdate(sql.toString(), get_TrxName());
+		log.fine("Set C_ConversionType_ID=" + no);
+		sql = new StringBuilder ("UPDATE I_Invoice ")
+				.append("SET I_IsImported='E', I_ErrorMsg=I_ErrorMsg||'ERR=Invalid ConversionTypeValue, ' ")
+				.append("WHERE C_ConversionType_ID IS NULL AND (ConversionTypeValue IS NOT NULL)")
+				.append(" AND I_IsImported<>'Y' ").append (clientCheck);
+		no = DB.executeUpdate(sql.toString(), get_TrxName());
+		if (no != 0)
+			log.warning ("Invalid ConversionTypeValue=" + no);
+		
 
-				//User1
-				sql = new StringBuilder ("UPDATE I_Invoice o ")
-						  .append("SET User1_ID=(SELECT C_ElementValue_ID FROM C_ElementValue c")
-						  .append(" WHERE o.User1Name=c.Name AND o.AD_Client_ID=c.AD_Client_ID) ")
-						  .append("WHERE User1_ID IS NULL AND User1Name IS NOT NULL AND I_IsImported<>'Y'").append (clientCheck);
-					no = DB.executeUpdate(sql.toString(), get_TrxName());
-					if (log.isLoggable(Level.FINE)) log.fine("Set User1=" + no);
-					// Set proper error message
-					sql = new StringBuilder ("UPDATE I_Invoice ")
-						  .append("SET I_IsImported='E', I_ErrorMsg=I_ErrorMsg||'ERR=Not Found User1_ID, ' ")
-						  .append("WHERE User1_ID IS NULL AND User1Name IS NOT NULL AND I_IsImported<>'Y'").append (clientCheck);
-					no = DB.executeUpdate(sql.toString(), get_TrxName());
-					if (no != 0)
-						log.warning("No User1Name=" + no);
+		//User1
+		sql = new StringBuilder ("UPDATE I_Invoice o ")
+				  .append("SET User1_ID=(SELECT MAX(C_ElementValue_ID) FROM C_ElementValue c "
+				  + "join C_Element e on c.C_Element_ID = e.C_Element_ID and e.ElementType = 'U' ")
+				  .append(" WHERE o.User1Name=c.Value AND o.AD_Client_ID=c.AD_Client_ID) ")
+				  .append("WHERE User1_ID IS NULL AND User1Name IS NOT NULL AND I_IsImported<>'Y'").append (clientCheck);
+			no = DB.executeUpdate(sql.toString(), get_TrxName());
+			if (log.isLoggable(Level.FINE)) log.fine("Set User1=" + no);
+			// Set proper error message
+			sql = new StringBuilder ("UPDATE I_Invoice ")
+				  .append("SET I_IsImported='E', I_ErrorMsg=I_ErrorMsg||'ERR=Not Found User1_ID, ' ")
+				  .append("WHERE User1_ID IS NULL AND User1Name IS NOT NULL AND I_IsImported<>'Y'").append (clientCheck);
+			no = DB.executeUpdate(sql.toString(), get_TrxName());
+			if (no != 0)
+				log.warning("No User1Name=" + no);
 			// David Castillo 04/10/2022
 			
 			//SetSalesRep 
@@ -954,8 +955,7 @@ public class ImportInvoice extends CustomProcess
 						invoice.setDateInvoiced(imp.getDateInvoiced());
 					if (imp.getDateAcct() != null)
 						invoice.setDateAcct(imp.getDateAcct());
-
-
+					
 					if (imp.get_ValueAsInt("User1_ID") > 0)
 						invoice.setUser1_ID(imp.get_ValueAsInt("User1_ID"));
 					//Conversion Type
@@ -1005,6 +1005,8 @@ public class ImportInvoice extends CustomProcess
 				// globalqss - [2855673] - assign dimensions to lines also in case they're different 
 				if (imp.getC_Activity_ID() != 0)
 					line.setC_Activity_ID(imp.getC_Activity_ID());
+				if (imp.get_ValueAsInt("User1_ID") > 0)
+					line.setUser1_ID(imp.get_ValueAsInt("User1_ID"));
 				if (imp.getC_Campaign_ID() != 0)
 					line.setC_Campaign_ID(imp.getC_Campaign_ID());
 				if (imp.getC_Project_ID() != 0)
